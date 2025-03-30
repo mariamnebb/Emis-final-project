1f9bc2e6586f2a72ee9e4ba63ef5f21161e25f17
Mariam Nebieridze <153996932+mariamnebb@users.noreply.github.com>
2025-03-30 15:45:23 +0400

    FINISH FETCH

--- script.js
+++ script.js
@@ -85,3 +85,91 @@ if(Object.keys(errors).length == 0){
 })
 
 // fetch 
+
+let currentPage = 1;
+let totalPages;
+
+function getUsers(page) {
+  fetch("https://reqres.in/api/users?page=" + page, {
+    method: "GET",
+  })
+    .then(function (response) {
+      if (response.status !== 200) {
+        throw response.status;
+      }
+      return response.json();
+    })
+    .then(function (responseData) {
+      // let container = document.getElementById('container')
+      let fragment = document.createDocumentFragment();
+      totalPages = responseData.total_pages;
+      console.log(responseData);
+
+      
+      responseData.data.forEach(function (item) {
+        let container = document.createElement("div"); 
+        container.style.textAlign = "center"; 
+        
+        let image = document.createElement("img");
+        image.src = item.avatar;
+        image.style.display = "block"; 
+        image.style.margin = "0 auto"; 
+        image.style.width = "150px"; 
+        image.style.height = "150px"; 
+        
+        let li = document.createElement("li");
+        li.textContent = item.email;
+        li.style.listStyle = "none"; 
+    
+        container.appendChild(image); 
+        container.appendChild(li); 
+    
+        fragment.appendChild(container); 
+    });
+    
+
+      document.getElementById("ul_list").innerHTML = " ";
+      document.getElementById("ul_list").appendChild(fragment);
+    })
+    .catch(function () {
+      let container = document.getElementById("container");
+      let p = document.createElement("p");
+      p.textContent = "server error 404";
+      container.appendChild(p);
+    });
+}
+
+document.getElementById("loadprev").addEventListener("click", function () {
+  if (currentPage === 1) {
+    return;
+  }
+
+  currentPage -= 1;
+  getUsers(currentPage);
+  
+});
+
+// let loadPrev = document.getElementById("loadprev");
+
+// if (loadPrev) {
+//   loadPrev.addEventListener("click", function () {
+//     if (currentPage === 1) {
+//       return;
+//     }
+//     currentPage -= 1;
+//     getUsers(currentPage);
+//   });
+// }
+
+
+document.getElementById("loadnext").addEventListener("click", function () {
+  if (currentPage === totalPages) {
+    return;
+  }
+  currentPage += 1;
+  getUsers(currentPage);
+});
+
+getUsers(currentPage);
+
+
