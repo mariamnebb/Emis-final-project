e1091e972fef0b11622cd120cb062f042c2c2140
Mariam Nebieridze <153996932+mariamnebb@users.noreply.github.com>
2025-03-31 17:11:56 +0400

    finish

--- script.js
+++ script.js
@@ -19,22 +19,22 @@
 // })
 
 
-burger.addEventListener('click', function(){
-if (navigation.classList.contains('activeNavigation')){
-    //ნავიგაციას აქრობს
-    navigation.classList.remove('activeNavigation')
-    //ბურგერ მენიუდ გადააქცევს x
-    burger.innerHTML= '<i class="fa-solid fa-bars"></i> '
-}
+// burger.addEventListener('click', function(){
+// if (navigation.classList.contains('activeNavigation')){
+//     //ნავიგაციას აქრობს
+//     navigation.classList.remove('activeNavigation')
+//     //ბურგერ მენიუდ გადააქცევს x
+//     burger.innerHTML= '<i class="fa-solid fa-bars"></i> '
+// }
 
-else {
-    //გამოიტანს ნავიგაციის მენიუს
-    navigation.classList.add('activeNavigation')
-    //გამოაჩენს x
-    burger.innerHTML ='<i class="fas fa-times"></i>'
-}
+// else {
+//     //გამოიტანს ნავიგაციის მენიუს
+//     navigation.classList.add('activeNavigation')
+//     //გამოაჩენს x
+//     burger.innerHTML ='<i class="fas fa-times"></i>'
+// }
 
-})
+// })
 
 // registractin valisation
 
