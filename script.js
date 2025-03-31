1db3ee17d8df388d09bcad2fb9a30812e130d9d8
Mariam Nebieridze <153996932+mariamnebb@users.noreply.github.com>
2025-03-31 15:23:05 +0400

    Finish version

--- script.js
+++ script.js
@@ -3,19 +3,39 @@
 
 // burgerbar
 
+// let navigation = document.getElementById('navigation')
+// let burger = document.getElementById('burger')
+// // აიქონზე დაკლიკებისას 
+// burger.addEventListener('click',function(){
+//     if(navigation.classList.contains('activeNavigation')){
+//         navigation.classList.remove('activeNavigation')
+//         burger.innerHTML =' <i class="fa-solid fa-bars"></i>'
+
+//     }else{
+//         navigation.classList.add('activeNavigation')
+//         burger.innerHTML= '<i class="fas fa-times"></i>'
+
+//     }
+// })
+
 let navigation = document.getElementById('navigation')
 let burger = document.getElementById('burger')
-// აიქონზე დაკლიკებისას 
-burger.addEventListener('click',function(){
-    if(navigation.classList.contains('activeNavigation')){
-        navigation.classList.remove('activeNavigation')
-        burger.innerHTML =' <i class="fa-solid fa-bars"></i>'
 
-    }else{
-        navigation.classList.add('activeNavigation')
-        burger.innerHTML= '<i class="fas fa-times"></i>'
+burger.addEventListener('click', function(){
+if (navigation.classList.contains('activeNavigation')){
+    //ნავიგაციას აქრობს
+    navigation.classList.remove('activeNavigation')
+    //ბურგერ მენიუდ გადააქცევს x
+    burger.innerHTML= '<i class="fa-solid fa-bars"></i> '
+}
 
-    }
+else {
+    //გამოიტანს ნავიგაციის მენიუს
+    navigation.classList.add('activeNavigation')
+    //გამოაჩენს x
+    burger.innerHTML ='<i class="fas fa-times"></i>'
+}
+
 })
 
 // registractin valisation
